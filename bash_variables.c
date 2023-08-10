021c9cf8f659990e9afff87fb43532d358937016
Omar <omartolba004@gmail.com>
2023-08-10 05:38:16 -0500

    adding bashvars files

--- bash_variables.c
+++ bash_variables.c
@@ -14,7 +14,6 @@
 /**********************************************************************************************************************
  *  INCLUDES
  *********************************************************************************************************************/
-
 #include "bash_variables.h"
 #include "data_structures.h"
 #include "functions.h"
@@ -23,18 +22,19 @@
 #include <stdio.h>
 
 /**********************************************************************************************************************
- *  GLOBAL VARIABLES
- *********************************************************************************************************************/
-
-/**********************************************************************************************************************
- *  STATIC FUNCTIONS
- *********************************************************************************************************************/
-
-/**********************************************************************************************************************
  *  FUNCTIONS IMPLEMENTATIONS
  *********************************************************************************************************************/
-void adding_bash_var(char **input, unsigned int index) /* index represents the location of token who has assignment operation*/
-{
+
+/*******************************************************************************
+ * Service Name: adding_bash_var
+ * Parameters (in):  input - pointer to pointer to char, index - pointer to unsigned char
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: void
+ * Description: Function to add bash variable to the data structure
+ *******************************************************************************/
+void adding_bash_var(char **input, unsigned int index)
+{				/* index represents the location of token who has assignment operation */
     struct bash_variable bashVar;
     char *name;
     char *value;
@@ -43,82 +43,80 @@ void adding_bash_var(char **input, unsigned int index) /* index represents the l
     unsigned int j = 0;
 
     while (input[index][j] != '=')
-        j++;
+	j++;
 
-    nameSize = j;                         /* assigning nameSize*/
-    valueSize = strlen(input[index]) - 1; /* assigning valueSize*/
+    nameSize = j;		/* assigning nameSize */
+    valueSize = strlen(input[index]) - 1;	/* assigning valueSize */
 
-    name = (char *)malloc(sizeof(char) * (nameSize + 1));   /* +1 for the null termiator*/
-    value = (char *)malloc(sizeof(char) * (valueSize + 1)); /* +1 for the null termiator*/
+    name = (char *) malloc(sizeof(char) * (nameSize + 1));	/* +1 for the null termiator */
+    value = (char *) malloc(sizeof(char) * (valueSize + 1));	/* +1 for the null termiator */
 
-    for (j = 0; j < nameSize; j++)
-    {
-        name[j] = input[index][j];
+    for (j = 0; j < nameSize; j++) {
+	name[j] = input[index][j];
     }
-    name[j] = '\0'; /* adding null terminator*/
+    name[j] = '\0';		/* adding null terminator */
 
-    for (j = 0; j < valueSize; j++)
-    {
-        value[j] = input[index][j + nameSize + 1];
+    for (j = 0; j < valueSize; j++) {
+	value[j] = input[index][j + nameSize + 1];
     }
-    value[j] = '\0'; /* adding null terminator*/
+    value[j] = '\0';		/* adding null terminator */
     bashVar.name = name;
     bashVar.value = value;
 
     insertAtLast(bashVar);
 
+    /* Freeing dynamically allocated memory to avoid memory leaks */
+    free(name);
+    free(value);
 }
 
+/*******************************************************************************
+ * Service Name: bash_variable_deref
+ * Parameters (in):  input - pointer to pointer to char, location - pointer to unsigned char
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: char **
+ * Description: Function for derefrencing bash variable
+ *  *******************************************************************************/
 char **bash_variable_deref(char **input, unsigned int index)
 {
-    char * value;
-    char * name;
+    char *name;
     unsigned char valueSize = 0;
-    unsigned char nameSize = 0; /*Variable to hold name size*/
-    unsigned int j =0;             /* Counter used to loop over sring*/
+    unsigned char nameSize = 0;	/*Variable to hold name size */
+    unsigned int j = 0;		/* Counter used to loop over sring */
 
-    if (input[index][j] != '$')
-    {
-        printf("unexpected error while derefrencing variable\n");
-        // exit(ER_WHILE_VARIABLE_DEREF);
+    if (input[index][j] != '$') {
+	printf
+	    ("unexpected error while derefrencing variable, exitting ..\n");
+	exit(ER_WHILE_VARIABLE_DEREF);
     }
 
-    j = 1;
+    j = 1;			/* Reassing j */
     while (input[index][j] != 0)
-        j++; /* counting name size*/
+	j++;			/* counting name size */
 
-    nameSize = j; /* assigning j to nameSize, +1 for null terminator*/
+    nameSize = j;		/* assigning j to nameSize, +1 for null terminator */
 
-    /* dynamically allocating size for name*/
-    name = (char *)malloc(sizeof(char) * (nameSize + 1));
+    /* dynamically allocating size for name */
+    name = (char *) malloc(sizeof(char) * (nameSize + 1));
 
-    for (j = 0; j < nameSize; j++)
-    {
-        name[j] = input[index][j + 1];
+    for (j = 0; j < nameSize; j++) {
+	name[j] = input[index][j + 1];
     }
 
-    name[j] = '\0'; /* adding null terminator*/
+    name[j] = '\0';		/* adding null terminator */
 
-    // printf("name is %s\n", name);
+    valueSize = return_valueSize_by_name(name);	/* calculating value string size */
 
-    valueSize = return_valueSize_by_name(name);
-
-    // printf("valus size %d\n", valueSize);
-
-    if (valueSize == 0)
-    {
-        printf("Variable doesn't exist\n");
-        return input; /* return input as it is*/
+    if (valueSize == 0) {
+	printf("Variable doesn't exist\n");
+	return input;		/* return input as it is */
     }
 
-
-    input[index]= (char *) malloc(sizeof(char)*valueSize);
+    input[index] = (char *) malloc(sizeof(char) * valueSize);
 
     input[index] = return_value_by_name(name);
 
-    // printf("value of name is %s and value of value is %s\n",name,input[index]);
-
     return input;
-    // free(name);
-    // free(value);
+    free(name);
 }
--- bash_variables.h
+++ bash_variables.h
@@ -15,27 +15,44 @@
 #ifndef BASH_VARIABLES_H
 #define BASH_VARIABLES_H
 
+/**********************************************************************************************************************
+ *  STRUCTURES
+ *********************************************************************************************************************/
 #include "data_structures.h"
 
 /**********************************************************************************************************************
  *  STRUCTURES
  *********************************************************************************************************************/
-struct bash_variable //TODO : is this structure has incomplete type
+struct bash_variable		//TODO : is this structure has incomplete type
 {
-    char * name; /* Name of the variable*/
-    char * value; /* Value of the variable*/
+    char *name;			/* Name of the variable */
+    char *value;		/* Value of the variable */
 };
 
-
-void adding_bash_var(char **input, unsigned int index);
-
-char ** bash_variable_deref(char **input, unsigned int index);
-
-
-
 /**********************************************************************************************************************
- *  EXTERNS
+ *  FUNCTIONS PROTOTYPES
  *********************************************************************************************************************/
 
+/*******************************************************************************
+ * Service Name: adding_bash_var
+ * Parameters (in):  input - pointer to pointer to char, index - pointer to unsigned char
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: void
+ * Description: Function to add bash variable to the data structure
+ *******************************************************************************/
+void adding_bash_var(char **input, unsigned int index);
+
+
+
+/*******************************************************************************
+ * Service Name: bash_variable_deref
+ * Parameters (in):  input - pointer to pointer to char, location - pointer to unsigned char
+ * Parameters (inout): None
+ * Parameters (out): None
+ * Return value: char **
+ * Description: Function for derefrencing bash variable
+ *  *******************************************************************************/
+char **bash_variable_deref(char **input, unsigned int index);
 
 #endif
